92bed0b799d581b0274149d4a76c50c27ea651cc
Tutanchamon <tutenchamon@gmail.com>
2014-04-21 14:43:10 +0200

    Wyswietlanie artykulow i cos tam jeszcze

--- wikitorpeda/revision.php
+++ wikitorpeda/revision.php
@@ -37,26 +37,9 @@
     <body style="margin-bottom: 90px;">
 
         <!-- Fixed navbar -->
-        <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Wikitorpeda</a>
-                </div>
-                <div class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#about">O nas</a></li>
-                        <li><a href="#contact">Kontakt</a></li>
-                    </ul>
-                </div><!--/.nav-collapse -->
-            </div>
-        </div>
+        <?php
+            require_once(dirname(__FILE__) . '/navbar.php');
+        ?>
 
         <div class="container" id="container">
             
