acfb6e6a9dfc1d02c5a9b9336c9ee8c179163819
Szymon Apolinarski <szymon.apolinarski@gmail.com>
2014-04-23 17:54:35 +0200

    logo

--- wikitorpeda/navbar.php
+++ wikitorpeda/navbar.php
@@ -8,7 +8,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">Wikitorpeda</a>
+                    <a class="navbar-brand" href="#"><img src="torpeda.png"></img></a>
                 </div>
                 <div class="navbar-collapse collapse">
                     <ul class="nav navbar-nav">
